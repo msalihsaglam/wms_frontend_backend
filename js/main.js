2fe6121ba1ed15bae8b0f4c6b4e18bcc4953ca39
Mehmet Salih Saglam <mehmetsalihsaglam@gmail.com>
2025-10-19 00:49:01 +0300

    Toggle button is added for navigation

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ function loadPage(page, el) {
   document.querySelectorAll('.sidebar a').forEach(a => a.classList.remove('active'));
   el.classList.add('active');
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleBtn = document.getElementById('toggleBtn');
+  const sidebar = document.getElementById('sidebar');
+  toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('collapsed');
+  });
+});
+
